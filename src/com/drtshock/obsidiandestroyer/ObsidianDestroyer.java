763185db9cbfbf3c28ea03b6444edc63c3b9a4f3
8rrico <8rrico@gmail.com>
2015-05-01 03:44:05 -0400

    ayyyeeee lmao

---
+++ src/com/drtshock/obsidiandestroyer/ObsidianDestroyer.java
@@ -0,0 +1,65 @@
+package com.drtshock.obsidiandestroyer;
+
+import com.drtshock.obsidiandestroyer.commands.ODCommand;
+import com.drtshock.obsidiandestroyer.listeners.BlockListener;
+import com.drtshock.obsidiandestroyer.listeners.EntityExplodeListener;
+import com.drtshock.obsidiandestroyer.listeners.PlayerListener;
+import com.drtshock.obsidiandestroyer.managers.ChunkManager;
+import com.drtshock.obsidiandestroyer.managers.ConfigManager;
+import com.drtshock.obsidiandestroyer.managers.MaterialManager;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
+
+public class ObsidianDestroyer extends JavaPlugin {
+
+    public static Logger LOG;
+    private static ObsidianDestroyer instance;
+
+    public static ObsidianDestroyer getInstance() {
+        return instance;
+    }
+
+    public static void debug(String debug) {
+        if (ConfigManager.getInstance() == null || ConfigManager.getInstance().getDebug()) {
+            LOG.info(debug);
+        }
+    }
+
+    public static void vdebug(String debug) {
+        if (ConfigManager.getInstance() == null || (ConfigManager.getInstance().getDebug() && ConfigManager.getInstance().getVerbose())) {
+            LOG.info(debug);
+        }
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        LOG = getLogger();
+
+        // Initialize managers
+        new ConfigManager(false);
+        new MaterialManager();
+        new ChunkManager();
+
+        // Set command executor
+        getCommand("od").setExecutor(new ODCommand());
+
+        // Register Event listeners
+        PluginManager pm = getServer().getPluginManager();
+
+        pm.registerEvents(new EntityExplodeListener(), this);
+        pm.registerEvents(new PlayerListener(), this);
+        pm.registerEvents(new BlockListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Save persistent data
+        if (ChunkManager.getInstance() != null) {
+            ChunkManager.getInstance().save();
+        }
+    }
+
+}
